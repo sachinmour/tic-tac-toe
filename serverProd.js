d9e4f1cf36e43cf2bf2944cac1ae82755c72fbc6
Sachin Mour <rtr.sachinmour@gmail.com>
2016-12-01 15:54:05 -0500

    production build

---
+++ serverProd.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _reload = require('reload');
+
+var _reload2 = _interopRequireDefault(_reload);
+
+var _index = require('./app/server/routes/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use(_express2.default.static(_path2.default.join(__dirname, '/app/client/public')));
+
+var server = _http2.default.createServer(app);
+
+if (process.env.NODE_ENV !== 'production') {
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
+  var webpack = require('webpack');
+  var config = require('./webpack.config.js');
+
+  var compiler = webpack(config);
+
+  var morgan = require('morgan');
+
+  app.use(morgan('dev'));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+  app.use(webpackHotMiddleware(compiler));
+  (0, _reload2.default)(server, app);
+}
+
+(0, _index2.default)(app);
+
+var PORT = process.env.PORT || 4545;
+server.listen(PORT, function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+  }
+});
